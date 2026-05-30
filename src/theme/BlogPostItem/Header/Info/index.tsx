89a43995dbc901d3fd192143687ca1af43b21d10
Mahdi Malverdi <mahdi.malverdi@gmail.com>
2026-05-30 14:05:26 +0330

    Remove unresolved theme-common imports

--- src/theme/BlogPostItem/Header/Info/index.tsx
+++ src/theme/BlogPostItem/Header/Info/index.tsx
@@ -1,52 +1,22 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 import React, {type ReactNode} from 'react';
 import clsx from 'clsx';
-import {translate} from '@docusaurus/Translate';
-import {usePluralForm} from '@docusaurus/theme-common';
-import {useDateTimeFormat} from '@docusaurus/theme-common/internal';
 import {useBlogPost} from '@docusaurus/plugin-content-blog/client';
 import type {Props} from '@theme/BlogPostItem/Header/Info';
 
 import styles from './styles.module.css';
 
-// Very simple pluralization: probably good enough for now
-function useReadingTimePlural() {
-  const {selectMessage} = usePluralForm();
-  return (readingTimeFloat: number) => {
-    const readingTime = Math.ceil(readingTimeFloat);
-    return selectMessage(
-      readingTime,
-      translate(
-        {
-          id: 'theme.blog.post.readingTime.plurals',
-          description:
-            'Pluralized label for "{readingTime} min read". Use as much plural forms (separated by "|") as your language support (see https://www.unicode.org/cldr/cldr-aux/charts/34/supplemental/language_plural_rules.html)',
-          message: 'One min read|{readingTime} min read',
-        },
-        {readingTime},
-      ),
-    );
-  };
-}
-
 function ReadingTime({readingTime}: {readingTime: number}) {
-  const readingTimePlural = useReadingTimePlural();
-  return <>{readingTimePlural(readingTime)}</>;
+  return <>{`${Math.ceil(readingTime)} min read`}</>;
 }
 
-function DateTime({
-  date,
-  formattedDate,
-}: {
-  date: string;
-  formattedDate: string;
-}) {
+function DateTime({date}: {date: string}) {
+  const formattedDate = new Intl.DateTimeFormat('en', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    timeZone: 'UTC',
+  }).format(new Date(date));
+
   return <time dateTime={date}>{formattedDate}</time>;
 }
 
@@ -83,19 +53,9 @@ export default function BlogPostItemHeaderInfo({className}: Props): ReactNode {
   const {date, readingTime} = metadata;
   const displayedReadingTime = getReadingTime({frontMatter, readingTime});
 
-  const dateTimeFormat = useDateTimeFormat({
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric',
-    timeZone: 'UTC',
-  });
-
-  const formatDate = (blogDate: string) =>
-    dateTimeFormat.format(new Date(blogDate));
-
   return (
     <div className={clsx(styles.container, 'margin-vert--md', className)}>
-      <DateTime date={date} formattedDate={formatDate(date)} />
+      <DateTime date={date} />
       {typeof displayedReadingTime !== 'undefined' && (
         <>
           <Spacer />
